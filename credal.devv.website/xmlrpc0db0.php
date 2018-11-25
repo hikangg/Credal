7fa40b833f0209a2e9bf602514b3965ada494a85
HiKangg <kangg1987@hotmail.com>
2018-11-25 01:06:23 -0800

    initial commit for deploy.

---
+++ credal.devv.website/xmlrpc0db0.php
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+  <service>
+    <engineName>WordPress</engineName>
+    <engineLink>https://wordpress.org/</engineLink>
+    <homePageLink>http://credal.devv.website</homePageLink>
+    <apis>
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://credal.devv.website/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://credal.devv.website/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://credal.devv.website/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://credal.devv.website/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://credal.devv.website/wp-json/" />
+	    </apis>
+  </service>
+</rsd>
